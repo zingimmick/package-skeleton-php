5f21924b1cc7cb920ff10ee4822fe98bfba7a54f
zingimmick <zingimmick@outlook.com>
2021-05-18 10:47:59 +0800

    Coding Standard 4

--- rector.php
+++ rector.php
@@ -2,38 +2,40 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Core\Configuration\Option;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Laravel\Set\LaravelSetList;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
+use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
+use Rector\Privatization\Rector\Property\PrivatizeLocalPropertyToPrivatePropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(RectorSetList::CUSTOM);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::SETS,
-        [
-            SetList::CODING_STYLE,
-            SetList::CODE_QUALITY,
-            SetList::CODE_QUALITY_STRICT,
-            SetList::DEAD_CODE,
-            SetList::PRIVATIZATION,
-            SetList::NAMING,
-            PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-            SetList::PHP_70,
-            SetList::PHP_71,
-            SetList::PHP_72,
-            SetList::EARLY_RETURN,
-        ]
-    );
     $parameters->set(
         Option::SKIP,
         [
-            FinalizeClassesWithoutChildrenRector::class,
-            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
             AddSeeTestAnnotationRector::class,
+            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
+            FinalizeClassesWithoutChildrenRector::class,
         ]
     );
     $parameters->set(
