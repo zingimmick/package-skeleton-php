0812e5446ffff8706806bea115b7b46c6b8ddef6
zingimmick <zingimmick@outlook.com>
2022-04-20 21:16:02 +0800

    Rector Rectify

--- rector.php
+++ rector.php
@@ -3,23 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (RectorConfig  $rectorConfig): void {
-    $rectorConfig->sets([
-
-        LevelSetList::UP_TO_PHP_72,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        RectorSetList::CUSTOM,
-
-    ]);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_72, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
     $rectorConfig->parallel();
     $rectorConfig->skip(
         [
@@ -28,7 +20,5 @@ return static function (RectorConfig  $rectorConfig): void {
             FinalizeClassesWithoutChildrenRector::class,
         ]
     );
-    $rectorConfig->paths(
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
-    );
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
